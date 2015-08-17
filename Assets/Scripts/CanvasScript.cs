3fc05f784fe322253d061198b5ed1d39ff130feb
dannielle <dannielle.del@gmail.com>
2015-08-17 09:44:59 -0400

    first scene, tree to cicada, cicada sound

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour {
+
+	Image img;
+	public Sprite cicadaSprite;
+	public Sprite a_tree;
+	public Sprite b_homeless;
+	public Sprite c_hill;
+	public Sprite d_hill;
+
+	void Start () {
+		img = gameObject.GetComponent<Image> ();
+	}
+	
+	void Update () {
+	
+	}
+
+	public void toCicada() {
+		printShit ();
+		img.sprite = cicadaSprite;
+	}
+
+	public void printShit() {
+		print ("print shit");
+	}
+
+	public void toNextBg() {
+
+	}
+}
